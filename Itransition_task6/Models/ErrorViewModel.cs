6c848cf7d9da56200b08817d70dee05443c7e53b
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2026-08-26 20:47:36 +0600

    Add project files.

---
+++ Itransition_task6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Itransition_task6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
